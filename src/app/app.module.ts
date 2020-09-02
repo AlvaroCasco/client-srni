7243e0570fdabe6b6edb4a453fcb7b78caa1da15
Engels Prado Herrera <engels.prado@alfa.com.ni>
2020-09-02 09:30:25 -0600

    error 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -68,7 +68,7 @@ import { HomeComponent } from './components/home/home.component';
           },
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('301893964236415'),
+            provider: new FacebookLoginProvider('617948235495154'),
             // provider: new FacebookLoginProvider('617948235495154'),
           },
           {
